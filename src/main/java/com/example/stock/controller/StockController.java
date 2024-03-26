37a6b404857a65a74696436b5c6f71f090f7de0f
AyeniAmanda <amandaayeni32@gmail.com>
2024-03-26 14:31:01 +0100

    application.properties changes and config to store the stock list

--- src/main/java/com/example/stock/controller/StockController.java
+++ src/main/java/com/example/stock/controller/StockController.java
@@ -2,13 +2,12 @@ package com.example.stock.controller;
 
 
 import com.example.stock.dto.StockDto;
-import com.example.stock.entity.Stock;
 import com.example.stock.service.StockService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-
-import java.math.BigDecimal;
 
 @RestController
 @RequestMapping("/api/stocks")
@@ -21,23 +20,27 @@ public class StockController {
     }
 
     @PostMapping
-    public Mono<StockDto> saveStock(@RequestBody StockDto stockDto) {
-        return stockService.save(stockDto);
+    public Mono<ResponseEntity<StockDto>> saveStock(@RequestBody StockDto stockDto) {
+        return stockService.save(stockDto)
+                .map(savedStockDto -> ResponseEntity.status(HttpStatus.CREATED).body(savedStockDto));
     }
 
     @PutMapping("/{id}")
-    public Mono<StockDto> updateStockPrice(@PathVariable long id, @RequestBody StockDto stockDto) {
-        return stockService.updateStockPrice(id, stockDto);
+    public Mono<ResponseEntity<StockDto>> updateStockPrice(@PathVariable long id, @RequestBody StockDto stockDto) {
+        return stockService.updateStockPrice(id, stockDto)
+                .map(updatedStockDto -> ResponseEntity.ok().body(updatedStockDto))
+                .defaultIfEmpty(ResponseEntity.notFound().build());
     }
 
     @GetMapping("/{id}")
-    public Mono<StockDto> getStockById(@PathVariable long id) {
-        return stockService.getStockById(id);
+    public Mono<ResponseEntity<StockDto>> getStockById(@PathVariable long id) {
+        return stockService.getStockById(id)
+                .map(stockDto -> ResponseEntity.ok().body(stockDto))
+                .defaultIfEmpty(ResponseEntity.notFound().build());
     }
 
     @GetMapping
     public Flux<StockDto> getAllStocks() {
         return stockService.getAllStocks();
     }
-
 }
